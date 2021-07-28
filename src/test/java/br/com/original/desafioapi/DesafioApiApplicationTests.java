cb6829e9b1e7c8718a8c7f0a122e15302f08b30f
Rodrigo Rocha <rodrigorocha@MacBook-Air-de-Rodrigo.local>
2021-07-28 15:33:19 -0300

    Criando o projeto.

---
+++ src/test/java/br/com/original/desafioapi/DesafioApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.original.desafioapi;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafioApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
